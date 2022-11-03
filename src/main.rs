0caa43277083808dde5af80bd3f9d5df9856bdb8
Romain Chardiny <romain.chardiny@gmail.com>
2022-11-03 10:33:40 +0100

    can provide port with env

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ const BASE_URL: &str = "http://localhost:8080/";
 const AVAILABLE_CHARS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const SHORT_ID_LEN: usize = 6;
 
+/// Generate a short ID
 fn generate_short_id() -> String {
     let mut rng = rand::thread_rng();
     let mut result = String::new();
@@ -55,6 +56,9 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+    let port_str = std::env::var("PORT").unwrap_or("8080".to_string());
+    let port: u16 = port_str.parse().expect("invalid port");
+
     let state = web::Data::new(AppState {
         urls: Mutex::new(HashMap::new()),
     });
@@ -66,7 +70,7 @@ async fn main() -> std::io::Result<()> {
             .service(url_redirection)
             .service(register)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
